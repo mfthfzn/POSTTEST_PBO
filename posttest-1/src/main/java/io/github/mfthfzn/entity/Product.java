02f65384a617c4edb7ae280fa2ab6b149885b036
mfthfzn <miftahulfauzan55@gmail.com>
2026-03-07 17:08:03 +0800

    posttest-1

---
+++ posttest-1/src/main/java/io/github/mfthfzn/entity/Product.java
@@ -0,0 +1,11 @@
+package io.github.mfthfzn.entity;
+
+public class Product {
+
+  String SKU;
+
+  String name;
+
+  String price;
+
+}
---
+++ posttest-1/src/main/java/io/github/mfthfzn/view/ProductView.java
@@ -0,0 +1,14 @@
+package io.github.mfthfzn.view;
+
+import io.github.mfthfzn.service.ProductService;
+
+public class ProductView {
+
+  ProductService productService;
+
+  public ProductView(ProductService productService) {
+    this.productService = productService;
+  }
+
+  
+}
